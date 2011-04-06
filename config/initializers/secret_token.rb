9291e376ae74552c3285216d2e95722f07f18fba
zeke <zeke@sikelianos.com>
2011-04-06 14:08:43 -0700

    vanilla rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sniph::Application.config.secret_token = 'a09e7ff109985b950afba62627407259260662842f86541396c3a21a9ebc0efcb4288b31be9b1221a59725248d007aee0fedc1169bdac3bf9edebaf67bd504f6'
