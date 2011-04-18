c3da2d4c354cd23fd40b41c464278270221160dd
zeke <zeke@sikelianos.com>
2011-04-17 19:26:55 -0700

    Improved querie#index

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -3,6 +3,7 @@ class QueriesController < ApplicationController
   def index
     @queries = Query.order('created_at DESC')
     @queries = @queries.limit(50)
+    @query_count = Query.count
     
     respond_to do |format|
       format.html
