3d8a7834233b5fdcba0347e00576b50b8984d107
zeke <zeke@sikelianos.com>
2011-04-20 21:04:20 -0700

    Prettied up the login nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     return content_tag(:span, number, :class => 'weak') if number.to_i.zero?
     number
   end
+
+  def user_nav
+    items = []
+    if logged_in?
+      items << current_user.nickname
+      items << link_to("Sign Out", signout_path)
+    else
+      items << link_to("Sign in with Twitter", "/auth/twitter")
+    end
+    content_tag(:ul, convert_to_list_items(items), :id => 'user_nav')
+  end
+
 end
