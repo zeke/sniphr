9303b601f4122aca237c97643b1651e993e9e7ea
zeke <zeke@sikelianos.com>
2011-04-26 22:39:39 -0700

    Fixed session_status response object structure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,21 +15,21 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, :notice => "Signed out!"
   end
-  
+
   # Didn't work if it was named 'status'..
   def session_status
     response_object = {}
     if logged_in?
       response_object[:msg] = "You are logged in as #{current_user.nickname}"
-      response_object[:user] = current_user
+      response_object[:user] = current_user.attributes
     else
       response_object[:msg] = "You are not logged in."
     end
-  
+
     respond_to do |format|
       format.json { render :json => response_object.to_json }
     end
-    
+
   end
 
 end
