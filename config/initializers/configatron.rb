111b89b8562869e676e18ac01522db56402f8691
zeke <zeke@sikelianos.com>
2011-05-08 10:55:10 -0700

    Set up secure config

--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -1,1 +1,2 @@
 configatron.configure_from_yaml(Rails.root.join('config', 'config.yml'))
+configatron.configure_from_yaml(Rails.root.join('config', 'config_secrets.yml')) # See config/config_secrets.yml.example
