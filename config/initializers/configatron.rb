253952a7a419eb4137e993101acaf38db7bf5357
zeke <zeke@sikelianos.com>
2011-05-08 11:05:15 -0700

    More configatron setup

--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -1,2 +1,4 @@
 configatron.configure_from_yaml(Rails.root.join('config', 'config.yml'))
-configatron.configure_from_yaml(Rails.root.join('config', 'config_secrets.yml')) # See config/config_secrets.yml.example
+
+secrets_file = Rails.root.join('config', 'config_secrets.yml')
+configatron.configure_from_yaml(secrets_file) if File.exist?(secrets_file)
