9c276eeb152ea7e7c7182505dfa227b26e41ea09
zeke <zeke@sikelianos.com>
2011-10-11 15:17:04 -0700

    configatron absorbs heroku config vars.

--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -2,3 +2,10 @@ configatron.configure_from_yaml(Rails.root.join('config', 'config.yml'))
 
 secrets_file = Rails.root.join('config', 'config_secrets.yml')
 configatron.configure_from_yaml(secrets_file) if File.exist?(secrets_file)
+
+if ENV['HEROKU']
+  configatron.twitter.key = ENV['TWITTER_KEY']
+  configatron.twitter.secret = ENV['TWITTER_SECRET']
+  configatron.facebook.key = ENV['FACEBOOK_KEY'] 
+  configatron.facebook.secret = ENV['FACEBOOK_SECRET']
+end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, (ENV['TWITTER_KEY'] || configatron.twitter.key), (ENV['TWITTER_SECRET'] || configatron.twitter.secret)
-  provider :facebook , (ENV['FACEBOOK_KEY'] || configatron.facebook.key), (ENV['FACEBOOK_SECRET'] || configatron.facebook.secret) , {:scope => "manage_pages"}
+  provider :twitter, configatron.twitter.key, configatron.twitter.secret
+  provider :facebook, configatron.facebook.key, configatron.facebook.secret, {:scope => "manage_pages"}
 end
